c2d8dc1e473feaf4988ea69a66f46e569d5d5e40
Srohit90 <rohitshinde43434@gmail.com>
2025-05-14 11:09:14 +0530

    DataGrabber

---
+++ WebApplication1/Controllers/DataGrabberController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataGrabberController1 : ControllerBase
+    {
+        public JsonResult SomeData()
+        {
+            return new JsonResult("Some Value");
+        }
+    }
+}
